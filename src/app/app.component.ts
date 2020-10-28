25bc488fc29c9b6f0fe843397c8e4e615e71de0f
ALEC_YAKSICH <ALEC_YAKSICH@midlandu.edu>
2020-10-28 11:23:14 -0500

    adding nav bar to log out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +10,16 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
- 
-  constructor(db: AngularFirestore) {
-  }
-  title = 'CurrencyApp';
+  ready = false;
+  user: Object;
+ constructor(private authservice: AuthService, private auth: AngularFireAuth){
+  this.auth.user.subscribe(v=>{ 
+    this.user = v;
+    this.ready = true
+  });
+ }
+
+ logout(){
+   this.authservice.logout();
+ }
 }
